972f53f1f7deb39f54809ac4f4cef484c8580514
Khaleque <kmshawon01850160659@gmail.com>
2023-10-01 09:21:02 +0600

    1st

---
+++ lib/screens/welcome_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
+import '../screens/screen.dart';
+import '../widgets/widget.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Flexible(
+                child: Column(
+                  children: [
+                    Center(
+                      child: Container(
+                        width:400,
+                        height:400,
+                        child: Image.network("https://m.gjcdn.net/fireside-post-image/900/7053344-ll-syhfwtaz-v4.webp"
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Welcome TO \nAndroid App Development",
+                      style: kHeadline,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                      "Anyone who stops learning is old, whether at twenty or eighty. Anyone who keeps learning stays young. The greatest thing in life is to keep your mind young. — Henry Ford",
+                        style: kBodyText,
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.grey[850],
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: MyTextButton(
+                        bgColor: Colors.white,
+                        buttonName: 'Register',
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                  builder: (context) => RegisterPage()));
+                        },
+                        textColor: Colors.black87,
+                      ),
+                    ),
+                    Expanded(
+                      child: MyTextButton(
+                        bgColor: Colors.transparent,
+                        buttonName: 'Sign In',
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                builder: (context) => SignInPage(),
+                              ));
+                        },
+                        textColor: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
